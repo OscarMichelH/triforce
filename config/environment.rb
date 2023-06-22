08ded23c9ae7ae78aa2554de0c74a7931c76d091
Oscar Michel <oscarmichelh@gmail.com>
2023-06-22 14:56:09 -0600

    Fixed domain name for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost:3000',
+  domain:               'womanshop.mx',
   user_name:            ENV['SMTP_USERNAME'],
   password:             ENV['SMTP_PASSWORD'],
   authentication:       'plain',
