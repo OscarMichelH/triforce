f6a8c23a531c18bdb0f0854ae3001876735a0121
Oscar Michel Herrera <oscarmichelh@gmail.com>
2024-06-25 17:48:32 -0600

    Added rutinas to page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :discounts
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-
   devise_scope :user do
 
     unauthenticated do
